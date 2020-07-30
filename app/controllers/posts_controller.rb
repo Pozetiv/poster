4da3bede2004090afc3096c12a9df47c437efe04
Roman <pozetivteam@gmail.com>
2020-07-30 23:52:33 +0300

    add CRUD posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,36 @@
+class PostsController < ApplicationController
+  before_action :set_post, except: %i[new create]
+  before_action :authenticate_user!
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.create(posts_params)
+  end
+
+  def edit; end
+
+  def update
+    if @post.update(posts_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    redirect_to posts_path if @post.destroy
+  end
+
+  private
+
+  def posts_params
+    params.require(:posts).permit(:title, :descriptions)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end
